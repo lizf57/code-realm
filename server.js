59f1acc241b88287472302e5bc8925d070d45c8f
mona lisa <lizfriedman5@gmail.com>
2023-09-20 18:33:55 -0500

    auth / helpers added; main handlebars added

--- server.js
+++ server.js
@@ -17,28 +17,28 @@ const hbs = exphbs.create({
     }
 })
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.set('views', './views');
-
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-      db: sequelize
+        db: sequelize
     })
-  };
+};
 
 app.use(session(sess))
 
-app.use(express.static("public"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'))
+    app.listen(PORT, () => console.log('Now listening at http://localhost:' + PORT))
 });
