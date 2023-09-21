a32ab609a8b96e1c22c0597e10ee7b78217e81f3
mona lisa <lizfriedman5@gmail.com>
2023-09-20 19:10:05 -0500

    added handlebars: main, home, login-out

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
+const path = require('path')
 const express = require('express');
 const session = require('express-session')
-const routes = require('./controllers')
 const exphbs = require('express-handlebars')
+const routes = require('./controllers')
 
 const sequelize = require('./config/connection')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
+
 const app = express()
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({
-    helpers: {
-        format_date: function(date) {
-            return new Date(date).toLocaleDateString()
-        }
-    }
-})
+const helpers = require('./utils/helpers')
+const hbs = exphbs.create({ helpers })
 
 const sess = {
     secret: 'Super secret secret',
