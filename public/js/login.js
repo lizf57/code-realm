225a9a79e1182125662d9bbb832b653a5d69aad9
mona lisa <lizfriedman5@gmail.com>
2023-10-06 20:31:46 -0500

    added post, dashboard, editPost, postInfo handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ async function loginFormHandler(event){
     const password = document.querySelector('#password-login').value.trim()
 
     if(username && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/login', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
